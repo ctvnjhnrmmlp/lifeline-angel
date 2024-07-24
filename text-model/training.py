e1a1a4c343af14ad78995b606addcf92bacdc14b
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-07-24 11:07:03 +0800

    feat: initial commit (included initial version of text model)

---
+++ text-model/training.py
@@ -0,0 +1,89 @@
+import random
+import json
+import pickle
+import numpy as np
+import nltk
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.optimizers import SGD
+
+# Initialize the lemmatizer
+lemmatizer = WordNetLemmatizer()
+
+# Load intents JSON file
+with open('intents.json') as file:
+    intents = json.load(file)
+
+# Create lists to hold words, classes, and documents
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', '.', ',']
+
+# Process intents and patterns
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        # Tokenize each word in the pattern
+        word_list = nltk.word_tokenize(pattern)
+        words.extend(word_list)
+        # Add the document to the list
+        documents.append((word_list, intent['tag']))
+        # Add the tag to the classes if it's not already there
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+# Lemmatize and sort the words, and remove duplicates
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+
+# Sort classes
+classes = sorted(set(classes))
+
+# Serialize words and classes using pickle
+with open('words.pkl', 'wb') as file:
+    pickle.dump(words, file)
+with open('classes.pkl', 'wb') as file:
+    pickle.dump(classes, file)
+
+# Create the training data
+training = []
+output_empty = [0] * len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([np.array(bag), np.array(output_row)])
+
+# Shuffle and convert the training data to a numpy array
+random.shuffle(training)
+training = np.array(training, dtype=object)
+
+# Split the training data into inputs and outputs
+train_x = np.array([element[0] for element in training])
+train_y = np.array([element[1] for element in training])
+
+# Build the neural network model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile the model
+sgd = SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Train the model
+hist = model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+
+# Save the trained model
+model.save('chatbot_model.h5', hist)
+print("Done")
