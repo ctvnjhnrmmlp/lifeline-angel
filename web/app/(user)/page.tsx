5f729be2f8c4ceacce784af092425c5c27b65de8
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-08-21 14:21:29 +0800

    fix: removed some controls on home page

--- web/app/(user)/page.tsx
+++ web/app/(user)/page.tsx
@@ -25,48 +25,14 @@ export default function Page() {
 		<main className='flex flex-col justify-center pl-[26rem] py-4 pr-4 h-screen w-screen'>
 			<section className='overflow-y-scroll no-scrollbar backdrop-blur-2xl bg-foreground/5 rounded-2xl h-screen'>
 				<div className='flex flex-col p-6 outline h-full'>
-					{/* Mini Navbar */}
-					<div className='w-full flex flex-col flex-wrap justify-between space-between gap-12'>
-						<div className='flex items-center justify-between'>
-							<div>
-								<p className='font-bold text-4xl'>New message</p>
-							</div>
-							<div className='flex space-x-2'>
-								<button className='block rounded-full p-3 bg-foreground text-background text-2xl'>
-									<FaCamera />
-								</button>
-								<button className='block rounded-full p-3 bg-foreground text-background text-2xl'>
-									<BsGrid1X2Fill />
-								</button>
-							</div>
-						</div>
-					</div>
 					{/* Messages */}
 					<div className='flex-grow w-full flex flex-col justify-center items-center'>
 						<div className='flex flex-col gap-4'>
 							<div>
-								<p className='text-3xl font-bold'>Waddup cuh</p>
+								<p className='text-4xl font-bold'>Waddup Cuh</p>
 							</div>
 						</div>
 					</div>
-					{/* Message */}
-					<div className='w-full flex justify-between space-x-3'>
-						<div className='w-full'>
-							<input
-								type='text'
-								placeholder='Aa'
-								className='p-3 w-full rounded-xl placeholder:font-bold placeholder:text-foreground font-bold text-foreground text-2xl'
-							/>
-						</div>
-						<div className='flex space-x-2'>
-							<button className='block rounded-full bg-foreground text-background text-2xl p-4'>
-								<FaMicrophone />
-							</button>
-							<button className='block rounded-full bg-foreground text-background text-2xl p-4'>
-								<FaLocationArrow />
-							</button>
-						</div>
-					</div>{' '}
 				</div>
 			</section>
 		</main>
