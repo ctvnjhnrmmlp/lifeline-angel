a5e53ec1154eb518a117c95dffa0fa9ae7d3b0d2
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-08-23 17:16:33 +0800

    fix: added improvements in sidebar

--- web/components/blocks/Button/ConversationButton.tsx
+++ web/components/blocks/Button/ConversationButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { getTimeDifference } from '@/utilities/functions';
 import { Conversation } from '@prisma/client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -8,14 +9,12 @@ import React from 'react';
 const ConversationButton = ({ conv }: { conv: Conversation }) => {
 	const pathname = usePathname();
 
-	console.log(pathname);
-
 	return (
 		<div
 			key={conv.id}
 			className='backdrop-blur-2xl bg-foreground/1 rounded-2xl shadow-2xl'
 			style={{
-				background: pathname.includes(conv.id) ? '#FFFFFF' : '',
+				background: pathname.includes(conv.id) ? '#27272A' : '',
 			}}
 		>
 			<Link href={`/conversation/${conv.id}`}>
@@ -24,7 +23,7 @@ const ConversationButton = ({ conv }: { conv: Conversation }) => {
 						<p
 							className='font-extrabold w-full text-xl text-foreground tracking-tight leading-none text-balance'
 							style={{
-								color: pathname.includes(conv.id) ? '#000000' : '',
+								color: pathname.includes(conv.id) ? '#FFFFFF' : '',
 							}}
 						>
 							{conv.title ? conv.title : 'New conversation'}
@@ -34,10 +33,10 @@ const ConversationButton = ({ conv }: { conv: Conversation }) => {
 						<p
 							className='font-light w-full text-sm text-zinc-700 tracking-tight leading-none text-ellipsis text-balance'
 							style={{
-								color: pathname.includes(conv.id) ? '#000000' : '',
+								color: pathname.includes(conv.id) ? '#FFFFFF' : '',
 							}}
 						>
-							Time
+							{getTimeDifference(conv.createdAt.toString())}
 						</p>
 					</div>
 				</div>
--- web/utilities/functions.ts
+++ web/utilities/functions.ts
@@ -9,3 +9,24 @@ export const checkTextValidURL = (text: string) => {
 
 	return url.protocol === 'http:' || url.protocol === 'https:';
 };
+
+export const getTimeDifference = (timestamp: string) => {
+	const now = new Date();
+	const time = new Date(timestamp);
+
+	const secondsAgo = Math.floor((now - time) / 1000);
+	const minutesAgo = Math.floor(secondsAgo / 60);
+	const hoursAgo = Math.floor(minutesAgo / 60);
+	const daysAgo = Math.floor(hoursAgo / 24);
+	const weeksAgo = Math.floor(daysAgo / 7);
+	const monthsAgo = Math.floor(daysAgo / 30);
+	const yearsAgo = Math.floor(daysAgo / 365);
+
+	if (minutesAgo < 60) return `${minutesAgo}m`;
+	if (hoursAgo < 24) return `${hoursAgo}h`;
+	if (daysAgo < 7) return `${daysAgo}d`;
+	if (weeksAgo < 4) return `${weeksAgo}w`;
+	if (monthsAgo < 12) return `${monthsAgo}mo`;
+
+	return `${yearsAgo}y`;
+};
