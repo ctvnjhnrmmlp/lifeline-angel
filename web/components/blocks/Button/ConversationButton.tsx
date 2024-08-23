e45635ccbd320855e3f33ea760991852b6ec6742
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-08-23 17:36:53 +0800

    fix: added improvements in sidebar

--- web/components/blocks/Button/ConversationButton.tsx
+++ web/components/blocks/Button/ConversationButton.tsx
@@ -13,7 +13,7 @@ const ConversationButton = ({ conv }: { conv: Conversation }) => {
 	return (
 		<div
 			key={conv.id}
-			className='backdrop-blur-2xl bg-foreground/1 rounded-2xl shadow-2xl hover:bg-zinc-800'
+			className='backdrop-blur-2xl bg-foreground/1 rounded-2xl shadow-2xl hover:bg-zinc-800 transition duration-100 ease-linear'
 			style={{
 				background: pathname.includes(conv.id) ? '#27272A' : '',
 			}}
@@ -32,20 +32,20 @@ const ConversationButton = ({ conv }: { conv: Conversation }) => {
 					</div>
 					<div className='flex items-center space-x-1'>
 						<p
-							className='font-light w-full text-sm text-zinc-700 tracking-tight leading-none text-ellipsis text-balance'
-							style={{
-								color: pathname.includes(conv.id) ? '#FFFFFF' : '',
-							}}
-						>
-							{getTimeDifference(conv.createdAt.toString())}
-						</p>
-						<p
 							className='text-zinc-700 text-xs'
 							style={{
 								color: pathname.includes(conv.id) ? '#FFFFFF' : '',
 							}}
 						>
 							<GoDotFill />
+						</p>
+						<p
+							className='font-light w-full text-sm text-zinc-700 tracking-tight leading-none text-ellipsis text-balance'
+							style={{
+								color: pathname.includes(conv.id) ? '#FFFFFF' : '',
+							}}
+						>
+							{getTimeDifference(conv.createdAt.toString())}
 						</p>
 					</div>
 				</div>
