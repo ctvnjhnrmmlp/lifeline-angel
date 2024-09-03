f08de6ebc97af4b5dc086e02b00eee8daccc0c83
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-09-03 18:35:52 +0800

    fix: added messages controls for animated model responses

--- web/components/blocks/Card/ModelAnimatedTextMessageCard.tsx
+++ web/components/blocks/Card/ModelAnimatedTextMessageCard.tsx
@@ -1,13 +1,46 @@
 'use client';
 
-import { convertDateTo24HourTimeFormat } from '@/utilities/functions';
+import { convertDateTo24HourTimeFormat, copy } from '@/utilities/functions';
 import { Message } from '@prisma/client';
+import { useState } from 'react';
+import { IoIosCopy } from 'react-icons/io';
+import { RiVoiceprintFill } from 'react-icons/ri';
+import { useTextToVoice } from 'react-speakup';
 
 const ModelAnimatedTextMessageCard = ({ message }: { message: Message }) => {
+	const [voiceMessageMode, setVoiceMessageMode] = useState('');
 	const formattedMessages = message.content
 		.split('.')
 		.map((point) => point.trim())
 		.filter((point) => point.length > 0);
+
+	const {
+		speak: speakMessage,
+		pause: pauseMessage,
+		resume: resumeMessage,
+		ref: messageRef,
+		setVoice,
+		voices,
+	} = useTextToVoice<HTMLDivElement>({
+		pitch: 1,
+		rate: 1,
+		volume: 1,
+	});
+
+	const handleSpeakMessage = () => {
+		setVoiceMessageMode('speaking');
+		speakMessage();
+	};
+
+	const handlePauseMessage = () => {
+		setVoiceMessageMode('paused');
+		pauseMessage();
+	};
+
+	const handleResumeMessage = () => {
+		setVoiceMessageMode('speaking');
+		resumeMessage();
+	};
 
 	return (
 		<div className='flex flex-col space-y-2'>
@@ -28,9 +61,53 @@ const ModelAnimatedTextMessageCard = ({ message }: { message: Message }) => {
 					</ul>
 				</div>
 			</div>
-			<p className='text-xs text-foreground tracking-tight leading-none text-ellipsis text-balance text-left'>
-				{convertDateTo24HourTimeFormat(message.createdAt.toString())}
-			</p>
+			<div className='flex items-center space-x-3'>
+				<div>
+					<p className='text-xs text-foreground tracking-tight text-ellipsis text-balance text-left'>
+						{convertDateTo24HourTimeFormat(message.createdAt.toString())}
+					</p>
+				</div>
+				<div className='flex items-center space-x-2'>
+					<div>
+						<button
+							className='text-foreground text-lg'
+							onClick={() => copy(message.content)}
+						>
+							<IoIosCopy />
+						</button>
+					</div>
+					{voiceMessageMode === '' && (
+						<div>
+							<button
+								className='text-foreground text-lg'
+								onClick={() => handleSpeakMessage()}
+							>
+								<RiVoiceprintFill />
+							</button>
+						</div>
+					)}
+					{voiceMessageMode === 'speaking' && (
+						<div>
+							<button
+								className='text-green-600 text-lg'
+								onClick={() => handlePauseMessage()}
+							>
+								<RiVoiceprintFill />
+							</button>
+						</div>
+					)}
+					{voiceMessageMode === 'paused' && (
+						<div>
+							<button
+								className='text-red-600 text-lg'
+								onClick={() => handleResumeMessage()}
+							>
+								<RiVoiceprintFill />
+							</button>
+						</div>
+					)}
+				</div>
+			</div>
 		</div>
 	);
 };
