2c253ee420ee9ac76dfe0b66aa91cd7937bed4f7
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-11-01 18:27:09 +0800

    fix: added changes

--- web-new/components/layouts/Sidebar/Sidebar.tsx
+++ web-new/components/layouts/Sidebar/Sidebar.tsx
@@ -325,31 +325,8 @@ export default function Sidebar({
 				<SidebarRail />
 			</SidebarUI>
 			<SidebarInset>
-				<header className='flex shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-4'>
-					<div className='flex items-center gap-2 px-4'>
-						{/* <Separator orientation='vertical' className='mr-2 h-4' /> */}
-						{/* <Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className='hidden md:block'>
-									<Link href='/'>Conversations</Link>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator className='hidden md:block' />
-								<BreadcrumbItem>
-									<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb> */}
-					</div>
-				</header>
-				<div className='flex flex-col gap-4 p-3'>
-					{children}
-					{/* <div className='grid auto-rows-min gap-4 md:grid-cols-3'>
-						<div className='aspect-video rounded-xl bg-muted/50' />
-						<div className='aspect-video rounded-xl bg-muted/50' />
-						<div className='aspect-video rounded-xl bg-muted/50' />
-					</div>
-					<div className='min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min' /> */}
-				</div>
+				<header className='flex shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-4'></header>
+				<div className='flex flex-col gap-4 p-3 no-scrollbar'>{children}</div>
 			</SidebarInset>
 		</SidebarProvider>
 	);
