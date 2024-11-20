76282c4b6bb55d8a5368d1f6b7d2cea8823743fa
ctvnjhnrmmlp <johnrommel.octaviano@gmail.com>
2024-11-20 22:12:53 +0800

    fix: added changes

--- web/app/manifest.ts
+++ web/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
 		theme_color: VARIABLES.theme,
 		icons: [
 			{
-				src: '../public/images/lifeline-angel.png',
+				src: '/images/lifeline-angel.png',
 				sizes: '192x192',
 				type: 'image/png',
 			},
 			{
-				src: '../public/images/lifeline-angel.png',
+				src: '/images/lifeline-angel.png',
 				sizes: '512x512',
 				type: 'image/png',
 			},
